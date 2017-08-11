29f3da18b012881c288f3e3b6bd17f698dccc219
任波 <borenfocus@gmail.com>
2017-08-11 14:00:08 +0800

    添加自定义字符串选择器

---
+++ BRPickerView/StringPickerView/BRStringPickerView.h
@@ -0,0 +1,47 @@
+//
+//  BRStringPickerView.h
+//  BRPickerViewDemo
+//
+//  Created by 任波 on 2017/8/11.
+//  Copyright © 2017年 renb. All rights reserved.
+//
+
+#import "BaseView.h"
+
+typedef void(^BRStringResultBlock)(id selectValue);
+
+@interface BRStringPickerView : BaseView
+
+/**
+ *  创建数组数据源PickerView
+ *
+ *  @param title            标题
+ *  @param dataSource       数组数据源
+ *  @param defaultSelValue  默认选中的行(单列传字符串，多列传一维数组)
+ *  @param isAutoSelect     是否自动选择，即选择完(滚动完)执行结果回调，传选择的结果值
+ *  @param resultBlock      选择后的回调
+ *
+ */
++ (void)showStringPickerWithTitle:(NSString *)title
+                       dataSource:(NSArray *)dataSource
+                  defaultSelValue:(id)defaultSelValue
+                     isAutoSelect:(BOOL)isAutoSelect
+                      resultBlock:(BRStringResultBlock)resultBlock;
+
+/**
+ *  创建数组数据源PickerView
+ *
+ *  @param title            标题
+ *  @param plistName        plist文件名
+ *  @param defaultSelValue  默认选中的行(单列传字符串，多列传一维数组)
+ *  @param isAutoSelect     是否自动选择，即选择完(滚动完)执行结果回调，传选择的结果值
+ *  @param resultBlock      选择后的回调
+ *
+ */
++ (void)showStringPickerWithTitle:(NSString *)title
+                        plistName:(NSString *)plistName
+                  defaultSelValue:(id)defaultSelValue
+                     isAutoSelect:(BOOL)isAutoSelect
+                      resultBlock:(BRStringResultBlock)resultBlock;
+
+@end
---
+++ BRPickerView/StringPickerView/BRStringPickerView.m
@@ -0,0 +1,315 @@
+//
+//  BRStringPickerView.m
+//  BRPickerViewDemo
+//
+//  Created by 任波 on 2017/8/11.
+//  Copyright © 2017年 renb. All rights reserved.
+//
+
+#import "BRStringPickerView.h"
+
+@interface BRStringPickerView ()<UIPickerViewDelegate,UIPickerViewDataSource>
+// 字符串选择器(默认大小: 320px × 216px)
+@property (nonatomic, strong) UIPickerView *pickerView;
+// 是否是单列
+@property (nonatomic, assign) BOOL isSingleColumn;
+// 数据源是否合法（数组的元素类型只能是字符串或数组类型）
+@property (nonatomic, assign) BOOL isDataSourceValid;
+@property (nonatomic, strong) NSString *title;
+@property (nonatomic, strong) NSArray  *dataSource;
+// 是否开启自动选择
+@property (nonatomic, assign) BOOL isAutoSelect;
+@property (nonatomic, copy) BRStringResultBlock resultBlock;
+// 单列选中的项
+@property (nonatomic, copy) NSString *selectedItem;
+// 多列选中的项
+@property (nonatomic, strong) NSMutableArray *selectedItems;
+
+@end
+
+@implementation BRStringPickerView
+
+#pragma mark - 显示StringPicker
++ (void)showStringPickerWithTitle:(NSString *)title
+                       dataSource:(NSArray *)dataSource
+                  defaultSelValue:(id)defaultSelValue
+                     isAutoSelect:(BOOL)isAutoSelect
+                      resultBlock:(BRStringResultBlock)resultBlock {
+    if (dataSource == nil || dataSource.count == 0) {
+        return;
+    }
+    BRStringPickerView *strPickerView = [[BRStringPickerView alloc]initWithTitle:title dataSource:dataSource defaultSelValue:defaultSelValue isAutoSelect:isAutoSelect resultBlock:resultBlock];
+    [strPickerView showWithAnimation:YES];
+}
+
+#pragma mark - 显示StringPicker
++ (void)showStringPickerWithTitle:(NSString *)title
+                        plistName:(NSString *)plistName
+                  defaultSelValue:(id)defaultSelValue
+                     isAutoSelect:(BOOL)isAutoSelect
+                      resultBlock:(BRStringResultBlock)resultBlock {
+    if (plistName == nil || plistName.length == 0) {
+        return;
+    }
+    NSString *path = [[NSBundle mainBundle] pathForResource:plistName ofType:@"plist"];
+    NSArray *dataSource =[[NSArray alloc] initWithContentsOfFile:path];
+    BRStringPickerView *strPickerView = [[BRStringPickerView alloc]initWithTitle:title dataSource:dataSource defaultSelValue:defaultSelValue isAutoSelect:isAutoSelect resultBlock:resultBlock];
+    [strPickerView showWithAnimation:YES];
+}
+
+
+#pragma mark - init
+- (instancetype)initWithTitle:(NSString *)title
+                   dataSource:(NSArray *)dataSource
+              defaultSelValue:(id)defaultSelValue
+                 isAutoSelect:(BOOL)isAutoSelect
+                  resultBlock:(BRStringResultBlock)resultBlock {
+    if (self = [super init]) {
+        self.title = title;
+        self.dataSource = dataSource;
+        self.isAutoSelect = isAutoSelect;
+        self.resultBlock = resultBlock;
+        
+        if (defaultSelValue) {
+            if ([defaultSelValue isKindOfClass:[NSString class]]) {
+                self.selectedItem = defaultSelValue;
+            } else if ([defaultSelValue isKindOfClass:[NSArray class]]){
+                self.selectedItems = [defaultSelValue mutableCopy];
+            }
+        }
+        
+        [self loadData];
+        [self initUI];
+    }
+    return self;
+}
+
+- (void)initUI {
+    [super initUI];
+    self.titleLabel.text = self.title;
+    // 添加字符串选择器
+    [self.alertView addSubview:self.pickerView];
+}
+
+#pragma mark - loadData
+- (void)loadData {
+    if (self.dataSource == nil || self.dataSource.count == 0) {
+        self.isDataSourceValid = NO;
+        return;
+    } else {
+        self.isDataSourceValid = YES;
+    }
+    
+    __weak typeof(self) weakSelf = self;
+    // 遍历数组元素 (遍历多维数组一般用这个方法)
+    [self.dataSource enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        static Class itemType;
+        if (idx == 0) {
+            itemType = [obj class];
+            // 判断数据源数组的第一个元素是什么类型
+            if ([obj isKindOfClass:[NSArray class]]) {
+                weakSelf.isSingleColumn = NO; // 非单列
+            } else if ([obj isKindOfClass:[NSString class]]) {
+                weakSelf.isSingleColumn = YES; // 单列
+            } else {
+                weakSelf.isDataSourceValid = NO; // 数组不合法
+                return;
+            }
+        } else {
+            // 判断数组的元素类型是否相同
+            if (itemType != [obj class]) {
+                weakSelf.isDataSourceValid = NO; // 数组不合法
+                *stop = YES;
+                return;
+            }
+            
+            if ([obj isKindOfClass:[NSArray class]]) {
+                if (((NSArray *)obj).count == 0) {
+                    weakSelf.isDataSourceValid = NO;
+                    *stop = YES;
+                    return;
+                } else {
+                    for (id subObj in obj) {
+                        if (![subObj isKindOfClass:[NSString class]]) {
+                            weakSelf.isDataSourceValid = NO;
+                            *stop = YES;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }];
+    
+    if (self.isSingleColumn) {
+        if (self.selectedItem == nil) {
+            // 如果是单列，默认选中数组第一个元素
+            self.selectedItem = _dataSource.firstObject;
+        }
+    } else {
+        BOOL isSelectedItemsValid = YES;
+        for (id obj in self.selectedItems) {
+            if (![obj isKindOfClass:[NSString class]]) {
+                isSelectedItemsValid = NO;
+                break;
+            }
+        }
+        
+        if (self.selectedItems == nil || self.selectedItems.count != self.dataSource.count || !isSelectedItemsValid) {
+            NSMutableArray *mutableArray = [NSMutableArray array];
+            for (NSArray* componentItem in _dataSource) {
+                [mutableArray addObject:componentItem.firstObject];
+            }
+            self.selectedItems = [NSMutableArray arrayWithArray:mutableArray];
+        }
+    }
+}
+
+- (void)didTapBackgroundView:(UITapGestureRecognizer *)sender {
+    [self dismissWithAnimation:NO];
+}
+
+/** 弹出视图方法 */
+- (void)showWithAnimation:(BOOL)animation {
+    //1. 获取当前应用的主窗口
+    UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
+    [keyWindow addSubview:self];
+    if (animation) {
+        // 动画前初始位置
+        CGRect rect = self.alertView.frame;
+        rect.origin.y = SCREEN_HEIGHT;
+        self.alertView.frame = rect;
+        
+        // 浮现动画
+        [UIView animateWithDuration:0.3 animations:^{
+            CGRect rect = self.alertView.frame;
+            rect.origin.y -= kDatePicHeight + kTopViewHeight;
+            self.alertView.frame = rect;
+        }];
+    }
+}
+
+/** 关闭视图方法 */
+- (void)dismissWithAnimation:(BOOL)animation {
+    // 关闭动画
+    [UIView animateWithDuration:0.2 animations:^{
+        CGRect rect = self.alertView.frame;
+        rect.origin.y += kDatePicHeight + kTopViewHeight;
+        self.alertView.frame = rect;
+        
+        self.backgroundView.alpha = 0;
+    } completion:^(BOOL finished) {
+        [self.leftBtn removeFromSuperview];
+        [self.rightBtn removeFromSuperview];
+        [self.titleLabel removeFromSuperview];
+        [self.lineView removeFromSuperview];
+        [self.topView removeFromSuperview];
+        [self.pickerView removeFromSuperview];
+        [self.alertView removeFromSuperview];
+        [self.backgroundView removeFromSuperview];
+        [self removeFromSuperview];
+        
+        self.leftBtn = nil;
+        self.rightBtn = nil;
+        self.titleLabel = nil;
+        self.lineView = nil;
+        self.topView = nil;
+        self.pickerView = nil;
+        self.alertView = nil;
+        self.backgroundView = nil;
+    }];
+}
+
+/** 取消按钮的点击事件 */
+- (void)clickLeftBtn {
+    [self dismissWithAnimation:YES];
+}
+
+/** 确定按钮的点击事件 */
+- (void)clickRightBtn {
+    NSLog(@"点击确定按钮后，执行block回调");
+    [self dismissWithAnimation:YES];
+    if(_resultBlock) {
+        if (self.isSingleColumn) {
+            _resultBlock([self.selectedItem copy]);
+        } else {
+            _resultBlock([self.selectedItems copy]);
+        }
+    }
+}
+
+- (UIPickerView *)pickerView {
+    if (!_pickerView) {
+        _pickerView = [[UIPickerView alloc]initWithFrame:CGRectMake(0, kTopViewHeight + 0.5, SCREEN_WIDTH, kDatePicHeight)];
+        _pickerView.backgroundColor = [UIColor whiteColor];
+        _pickerView.dataSource = self;
+        _pickerView.delegate = self;
+        
+        __weak typeof(self) weakSelf = self;
+        if (self.isSingleColumn) {
+            [_dataSource enumerateObjectsUsingBlock:^(NSString *rowItem, NSUInteger rowIdx, BOOL *stop) {
+                if ([weakSelf.selectedItem isEqualToString:rowItem]) {
+                    [weakSelf.pickerView selectRow:rowIdx inComponent:0 animated:NO];
+                    *stop = YES;
+                }
+            }];
+        } else {
+            [self.selectedItems enumerateObjectsUsingBlock:^(NSString *selectedItem, NSUInteger component, BOOL *stop) {
+                [_dataSource[component] enumerateObjectsUsingBlock:^(id rowItem, NSUInteger rowIdx, BOOL *stop) {
+                    if ([selectedItem isEqualToString:rowItem]) {
+                        [weakSelf.pickerView selectRow:rowIdx inComponent:component animated:NO];
+                        *stop = YES;
+                    }
+                }];
+            }];
+        }
+    }
+    return _pickerView;
+}
+
+
+#pragma mark - UIPickerViewDataSource
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    if (self.isSingleColumn) {
+        return 1;
+    } else {
+        return _dataSource.count;
+    }
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    if (self.isSingleColumn) {
+        return _dataSource.count;
+    } else {
+        return ((NSArray*)_dataSource[component]).count;
+    }
+}
+
+#pragma mark - UIPickerViewDelegate
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if (self.isSingleColumn) {
+        return _dataSource[row];
+    } else {
+        return ((NSArray*)_dataSource[component])[row];
+    }
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    if (self.isSingleColumn) {
+        self.selectedItem = _dataSource[row];
+    } else {
+        self.selectedItems[component] = ((NSArray *)_dataSource[component])[row];
+    }
+    // 设置是否自动回调
+    if (self.isAutoSelect) {
+        if(_resultBlock) {
+            if (self.isSingleColumn) {
+                _resultBlock([self.selectedItem copy]);
+            } else {
+                _resultBlock([self.selectedItems copy]);
+            }
+        }
+    }
+}
+
+@end
