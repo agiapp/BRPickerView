487cf55c4a94a450177576d51386b7665d16a4d1
renb <91renb@gmail.com>
2018-03-29 14:18:36 +0800

    更新代码

--- BRPickerViewDemo/Classes/TestViewController.m
+++ BRPickerViewDemo/Classes/TestViewController.m
@@ -189,7 +189,7 @@
         _birthdayTF.placeholder = @"请选择";
         __weak typeof(self) weakSelf = self;
         _birthdayTF.tapAcitonBlock = ^{
-            [BRDatePickerView showDatePickerWithTitle:@"出生日期" dateType:BRDatePickerModeYMD defaultSelValue:@"2018-03-21" minDateStr:@"2010-03-21 00:00:00" maxDateStr:@"2020-03-28 09:20:27" isAutoSelect:YES themeColor:nil resultBlock:^(NSString *selectValue) {
+            [BRDatePickerView showDatePickerWithTitle:@"出生日期" dateType:BRDatePickerModeYMDHM defaultSelValue:@"2018-03-21 10:00" minDateStr:@"2015-05-02 09:09:09" maxDateStr:@"2020-10-28 13:39:39" isAutoSelect:YES themeColor:nil resultBlock:^(NSString *selectValue) {
                 weakSelf.birthdayTF.text = selectValue;
             } cancelBlock:^{
                 NSLog(@"点击了背景或取消按钮");
